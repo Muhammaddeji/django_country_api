1788b49d8eb4199ad975059ef406d286df53de76
Muhammaddeji <adegbitesheriff300@gmail.com>
2026-01-08 15:41:19 +0100

    django_country_api

--- django_country_api/wsgi.py
+++ django_country_api/wsgi.py
@@ -3,7 +3,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'django_country_api.settings'
+    'gunicorn django_country_api.wsgi:application'
+
 )
 
 application = get_wsgi_application()
